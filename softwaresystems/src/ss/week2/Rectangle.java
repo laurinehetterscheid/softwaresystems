23f8b0077ac44db92f8c7b8cdb9d639edc88319d
Laurine Hetterscheid <laurine.hetterscheid@nedap.com>
2018-11-20 13:13:57 +0100

    Add Rectangle

--- softwaresystems/src/ss/week2/Rectangle.java
+++ softwaresystems/src/ss/week2/Rectangle.java
@@ -1,40 +1,57 @@
 package ss.week2;
 
 public class Rectangle {
-    private int length;
-    private int width;
+	
+    private int length; 	// current length
+    						// invariant: length > 0
+    private int width;		// current width
+							// invariant: width > 0
 
     /**
      * Create a new Rectangle with the specified length and width.
+     * @require length > 0
+     * 			width > 0
      */
-    public Rectangle(int length, int width) {
+    
+    /*@ pure */ public Rectangle(int length, int width) {
+    	this.length = length;
+    	this.width = width;
+
     }
     
     /**
      * The length of this Rectangle.
+     * @param length length of the <code>Rectangle</code>;
+     * @ ensures \result > 0;
      */
-    public int length() {
-	return 0;
+    /*@ pure */ public int length() {
+    	return length;
     }
 
     /**
      * The width of this Rectangle.
+     * @param width width of the <code>Rectangle</code>;
+     * @ ensures \result > 0;
      */
-    public int width() {
-    	return 0;
+    /*@ pure */ public int width() {
+    	return width;
     }
 
     /**
      * The area of this Rectangle.
+     * @ ensures \result > 0;
      */
-    public int area() {
-    	return 0;
+    /*@ pure */ public int area() {
+    	int area = length * width;
+    	return area;
     }
 
     /**
      * The perimeter of this Rectangle.
+     * @ ensures \result > 0;
      */
-    public int perimeter() {
-    	return 0;
+    /*@ pure */ public int perimeter() {
+    	int perimeter = 2 * (length + width);
+    	return perimeter;
     }
 }
